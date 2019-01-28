192bbc6510eb21887f57d53e4798c3bf3c0e5789
araly <araly.isaac@protonmail.com>
2019-01-28 15:56:17 +0100

    scraping: work on results cleaning

--- castle/index.js
+++ castle/index.js
@@ -3,7 +3,41 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var fetchUrl = require('fetch').fetchUrl;
+var readline = require('readline');
 var app = express();
+
+async function processLineByLine() {
+    const fileStream = fs.createReadStream('raw-output.html');
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+    // Note: we use the crlfDelay option to recognize all instances of CR LF
+    // ('\r\n') in input.txt as a single line break.
+
+    const appendStream = fs.open(); // TODO
+
+    var reading = 0;
+    for await (const line of rl) {
+        // Each line in input.txt will be successively available here as `line`.
+        console.log(`Line from file: ${line}`);
+
+        if (reading == 0 && line.contains('oMapOptionsdestinationfranceMap')) {
+            reading = 1;
+
+        }
+        else if (reading == 1 && line.contains('markers')) {
+            reading = 2;
+        }
+        else if (reading == 2 && !line.contains('</script>')) {
+            // TODO
+        }
+        else {
+            reading = 3;
+        }
+    }
+}
 
 app.get('/naive', function(req, res) {
 
@@ -46,8 +80,8 @@ app.get('/fetch', function(req, res) {
     var fetchResult;
 
     fetchUrl("https://www.relaischateaux.com/us/destinations/europe/france", {"credentials":"include","headers":{"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","accept-language":"en-US,en;q=0.9","upgrade-insecure-requests":"1"},"referrer":"https://www.relaischateaux.com/us/destinations/europe","referrerPolicy":"origin-when-cross-origin","body":null,"method":"GET","mode":"cors"}, function(error, meta, body) {
-        fs.writeFile('raw-output.json', body, function(err) {
-            console.log('raw-output.json successfully written');
+        fs.writeFile('raw-output.html', body, function(err) {
+            console.log('raw-output.html successfully written');
         });
     });
 });
