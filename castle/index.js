a3925ca318d18de198fb9403c804b5484a919e76
araly <araly.isaac@protonmail.com>
2019-02-17 18:46:12 +0100

    scraping: got a list of all restaurants of france

--- castle/index.js
+++ castle/index.js
@@ -6,7 +6,9 @@ var fetchUrl = require('fetch').fetchUrl;
 var readline = require('readline');
 var app = express();
 
-async function processLineByLine() {
+var restaurants
+
+async function processPseudoJSONLineByLine() { //unfinished, maybe will finish it later if I have time
     const fileStream = fs.createReadStream('raw-output.html');
 
     const rl = readline.createInterface({
@@ -39,9 +41,102 @@ async function processLineByLine() {
     }
 }
 
+function processHTMLLineByLineWithEvents() {
+    const rl = readline.createInterface({
+        input: fs.createReadStream('./raw-etablissements.html'),
+        clrfDelay: Infinity
+    });
+
+    restaurants = {};
+    var lastRestaurant = null;
+
+    var reading = 0;
+    rl.on('line', (line) => { // using on line event
+        if (reading == 0 && line.includes('<h3>France</h3>')) {
+            reading = 1;
+            console.log('found France');
+        }
+        else if (reading == 1 && !(line.includes('h3') || line.includes('<') || line.includes(';'))) {
+            console.log(`restaurant line: ${line.slice(44)}`);
+            if (!line.includes('    Chef - ') && !line.includes('    Maître de maison - ')) {
+                lastRestaurant = line.slice(44);
+                restaurants[lastRestaurant] = {};
+            }
+            else if (line.includes('    Chef - ')) {
+                restaurants[lastRestaurant].chef = line.slice(55);
+            }
+            else if (line.includes('    Maître de maison - ')) {
+                restaurants[lastRestaurant].maitre = line.slice(67);
+            }
+        }
+        else if (reading == 1 && line.includes('h3')) {
+            reading = 2;
+            console.log(`h3 line: ${line}`);
+            rl.close();
+        }
+    });
+
+    rl.on('close', () => {
+        console.log('close');
+        return;
+    });
+}
+
+async function processHTMLLineByLineWithForLoop() {
+    const rl = readline.createInterface({
+        input: fs.createReadStream('./raw-etablissements.html'),
+        clrfDelay: Infinity
+    });
+
+    restaurants = {};
+    var lastRestaurant = null;
+
+    var reading = 0;
+    for await (const line of rl) {
+        if (reading == 0 && line.includes('<h3>France</h3>')) {
+            reading = 1;
+            console.log('found France');
+        }
+        else if (reading == 1 && !(line.includes('h3') || line.includes('<') || line.includes(';'))) {
+            console.log(`restaurant line: ${line.slice(44)}`);
+            if (!line.includes('    Chef - ') && !line.includes('    Maître de maison - ')) {
+                lastRestaurant = line.slice(44);
+                restaurants[lastRestaurant] = {};
+            }
+            /*
+            else if (line.includes('    Chef - ')) {
+                restaurants[lastRestaurant].chef = line.slice(55);
+                console.log(restaurants[lastRestaurant]);
+                console.log(restaurants);
+            }
+            else if (line.includes('    Maître de maison - ')) {
+                restaurants[lastRestaurant].maitre = line.slice(67);
+                console.log(restaurants[lastRestaurant]);
+            }
+            */
+        }
+        else if (reading == 1 && line.includes('h3')) {
+            reading = 2;
+            console.log(`h3 line: ${line}`);
+            console.log(restaurants);
+            var toWrite = JSON.stringify(restaurants);
+            fs.writeFile("./etablissements.json", toWrite, 'utf8', function (err) {
+                if (err) {
+                    console.log("An error occured while writing JSON Object to File.");
+                    return console.log(err);
+                }
+                console.log("JSON file has been saved.");
+            });
+            break;
+        }
+    }
+}
+
+
+
 app.get('/naive', function(req, res) {
 
-    // All the web scraping magic will happen here
+    // All the web scraping magic will happen here // or not, that was a bad idea, the website is not pretty and well done, has a ton of things going all around
 
     // URL of thing to scrap
     url = 'https://www.relaischateaux.com/us/destinations/europe';
@@ -84,6 +179,19 @@ app.get('/fetch', function(req, res) {
             console.log('raw-output.html successfully written');
         });
     });
+});
+
+app.get('/etablissements', function(req, res) {
+    fetchUrl("https://www.relaischateaux.com/fr/site-map/etablissements", {"credentials":"include","headers":{"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","accept-language":"en-GB,en-US;q=0.9,en;q=0.8","cache-control":"max-age=0","upgrade-insecure-requests":"1"},"referrerPolicy":"no-referrer-when-downgrade","body":null,"method":"GET","mode":"cors"}, function(error, meta, body) {
+        fs.writeFile('raw-etablissements.html', body, function(err) {
+            console.log('raw-etablissements.html successfully written');
+        });
+    });
+});
+
+app.get('/process', function(req, res) {
+    processHTMLLineByLineWithForLoop();
+    console.log(`restaurants: ${restaurants}`);
 });
 
 app.get('/', (req, res) =>
